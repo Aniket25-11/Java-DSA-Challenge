bc0130b657e65d5b0f672f7a0075b6ae25a61176
Aniket25-11 <Www.aniketbhojapure@gmail.com>
2025-03-06 16:30:18 +0530

    Methods and Scope Completed - which is in Day 19 (most boring)

--- Day19/Main.java
+++ Day19/Main.java
@@ -28,3 +28,48 @@ public class Main {
         int a = 10;
     }
 }
+
+// 2. Method level of Scope 
+class Algebra1{
+    int a = 10; //Member Variables
+    int b = 5;
+
+    int add(){
+        int a = 20;
+        int b = 25;
+        return a + b;
+    }
+}
+
+
+// 3. Block Level Scope of Variables :-
+// Def:- We can access variables only inside the block . 
+
+// void demo(){
+//     int a = 10;
+//     System.out.println(a);
+
+//     System.out.println(b);
+//     {
+//         int b = 10;  // we can only access b here !
+//     }
+// }
+
+
+// 4. Formal Parameters and Actual Parameters 
+// ** Formal Parameters :- Which can defined during function definition.
+// It is also called as Just Parameters
+
+// ** Actual Parameters :- When will call any function and at the time of passing that parameters are called as Actual parameters. 
+// It is also called as Arguments. 
+
+
+// 5. Pass by Values & Pass by Reference. 
+
+
+
+
+
+// ***** This is the most Boring Topic *****
+
+
